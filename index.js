a0aba251f4b98d36a87ceb44d6dc6e41497b246c
Marcus Õunapuu <u-40@tptlive.ee>
2023-01-30 15:02:29 +0200

    add template

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require('express')
 const path = require('path')
 const app = express();
-const port = 3000
+const port = 3000;
+const nunjucks = require('nunjucks');
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.render('.index.njk');
 })
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.render('.about.njk');
+})
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.render('.contact.njk');
+})
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.render('.gallery.njk');
+})
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.render('./values.njk');
+})
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.render('.gallery.njk');
 })
